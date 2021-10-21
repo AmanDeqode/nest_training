d930654385934c195ddab75a93ae01cd889eae9a
Aman Chaudhary <achoudhary@deqode.com>
2021-10-21 12:39:53 +0530

    Add: match role function to check valid role

--- src/users/auth/roles.guards.ts
+++ src/users/auth/roles.guards.ts
@@ -1,20 +1,21 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
+
+const matchRoles = (roles,userRoles) => {
+  return roles.some(role => role === userRoles);
+}
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
-  canActivate(context: ExecutionContext): boolean| Promise<boolean> | Observable<boolean> {
+  canActivate(context: ExecutionContext): boolean {
     const roles = this.reflector.get<string[]>('roles', context.getHandler());
     if (!roles) {
       return true;
     }
     const request = context.switchToHttp().getRequest();
-    console.log(request)
     const user = request.user;
-    //return matchRoles(roles, user.roles);
-    return true;
+    return matchRoles(roles, user.role);
   }
 }
