99997c2fdef98e4722d9ffe4cb883b78d91fb9e3
Aman Chaudhary <achoudhary@deqode.com>
2021-10-21 11:07:28 +0530

    Add: entities and onetomany relation

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,24 @@
+import { Todo } from "src/todos/entities/todo.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Role } from "./role.enum";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id:string;
+
+    @Column({type:'varchar',nullable:false})
+    name:string;
+
+    @Column({type:"varchar",nullable:false,unique:true})
+    email:string;
+
+    @Column({type:"varchar",nullable:false})
+    password:string
+
+    @Column({ type: 'enum', enum: Role, default: Role.USER })
+    role:Role
+
+    @OneToMany('Todo',(task:Todo)=> task.user,{onDelete:'CASCADE',onUpdate:'CASCADE'})
+    tasks:Todo[]
+}
