c90de23eb734d4b3ad17563b1e04fba0c1a71802
Aman Chaudhary <achoudhary@deqode.com>
2021-10-22 17:16:22 +0530

    Add:class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,23 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
 import { Todo } from "src/todos/entities/todo.entity";
 import { Role } from "../entities/role.enum";
 
 export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
     name:string;
+
+    @IsEmail()
     email:string;
+    
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(7)
     password:string;
+
     tasks?:Todo[];
-    role:Role
+
+    /* @IsString()
+    @IsNotEmpty()
+    role:Role */
 }
