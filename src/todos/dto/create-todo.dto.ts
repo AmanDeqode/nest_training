7e7a65fef414a913a47fb30a8ad52e2e808e4b17
Aman Chaudhary <achoudhary@deqode.com>
2021-10-22 20:45:38 +0530

    Add:class-validator

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,7 +1,9 @@
+import { IsNotEmpty } from "class-validator";
 import { User } from "src/users/entities/user.entity";
 
 export class CreateTodoDto {
     id:string;
     user:User;
+    @IsNotEmpty()
     tasks:string[];
 }
